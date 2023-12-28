f73956c5f18e03560f6db475df82e05735c9af09
Niariasve <nestorariasve@gmail.com>
2023-12-27 20:44:10 -0500

    MVC y clase Tree

---
+++ src/main/java/arboles/Tree.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package arboles;
+
+/**
+ *
+ * @author User
+ */
+public class Tree<E> {
+    TreeNode<E> root;
+    
+    private class TreeNode<E> {
+        E contenido;
+        
+    } 
+}
