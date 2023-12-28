d9cdfd90f518cf61b73e21a3bff4a3529ccbc15d
Niariasve <nestorariasve@gmail.com>
2023-12-27 21:04:35 -0500

    Tree

--- src/main/java/arboles/Tree.java
+++ src/main/java/arboles/Tree.java
@@ -4,6 +4,9 @@
  */
 package arboles;
 
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  *
  * @author User
@@ -11,8 +14,43 @@ package arboles;
 public class Tree<E> {
     TreeNode<E> root;
     
+    public Tree(E contenido) {
+        this.root = new TreeNode<>(contenido);
+    }
+    
+    public Tree() {}
+    
+    public boolean isEmpty() {
+        return root == null;
+    }
+    
+    public boolean isLeaf() {
+        return root.children.isEmpty();
+    }
+    
+    public E getRoot() {
+        return root.contenido;
+    }
+    
+    public void setRoot(E e) {
+        root.contenido = e;
+    }
+    
+    public List<Tree> getChildren() {
+        return root.children;
+    }
+    
+    public void setChild(Tree child) {
+        root.children.add(child);
+    }
+    
     private class TreeNode<E> {
         E contenido;
+        List<Tree> children;
         
+        TreeNode(E contenido) {
+            this.contenido = contenido;
+            children = new LinkedList<>();
+        }       
     } 
 }
